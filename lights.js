4b840475b93a456a9df055138d2e1fd348ff74bc
shahmilshahidan <shahmilshahidan@Shahmils-Mac-mini.local>
2024-07-15 21:11:18 +0800

    Add onclick event to generate the boxes. Add stylings on the component in the pages.

--- lights.js
+++ lights.js
@@ -14,10 +14,40 @@ function random(number) {
   return Math.floor(Math.random() * (number + 1));
 }
 
-btn.addEventListener("click", () => {
+function startButton() {
+	let x = document.querySelector("button");
+	//TODO:Replace below event with table 1x4
+	x.addEventListener("click", createTable());
+	//x.addEventListener("click", () => {
+  	//	const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  	//	document.body.style.backgroundColor = rndCol;
+	//});
+}
+
+function createTable() {
+	const table = document.createElement("table");
+	const tbody = document.createElement("tbody");
+	
+	for (let i = 0; i < 1; i++){
+		const row = document.createElement("tr");
+
+		for (let j = 0; j < 4; j++){
+			const cell = document.createElement("td");
+			const cellText = document.createTextNode(`row ${i}, col ${j}`);
+			cell.appendChild(cellText);
+			row.appendChild(cell);
+		}
+
+		tbody.appendChild(row);
+	}
+
+	table.appendChild(tbody);
+	document.body.appendChild(table);
+	table.setAttribute("border", "2");
+
+}
+
+circle.addEventListener("click", () => {
   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 });
-
-//console.log(width);
-console.log(lights);
